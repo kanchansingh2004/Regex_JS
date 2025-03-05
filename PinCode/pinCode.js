e490f52dafc70cae78904e1106dfa1babad62ab9
kanchansingh2004 <kannursingh2004@gmail.com>
2025-03-06 00:24:08 +0530

    [Kanchan] Adding validation for PIN-400088

---
+++ PinCode/pinCode.js
@@ -0,0 +1,25 @@
+//Create class for pin code
+class Pincode{
+    constructor(pincode){
+        //Validate pincode
+        if(!this.validatePincode(pincode)){
+            throw "Invalid pincode";
+        }
+        this.pincode = pincode;
+    }
+    //Validate pincode
+    validatePincode(pincode){
+        return /^[1-9][0-9]{5}$/.test(pincode);
+        }
+    
+    
+}
+
+try{
+    let pincode = new Pincode("400088");
+    console.log("Valid PIN ");
+    console.log(pincode);
+}catch(e){
+    console.log(e);
+    }
+
